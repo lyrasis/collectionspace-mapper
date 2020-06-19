18f95f758dec564982b541e025375da1ad7e8b8b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-06-19 15:55:33 -0400

    initial gem structure

---
+++ lib/collectionspace/mapper.rb
@@ -0,0 +1,8 @@
+require "collectionspace/mapper/version"
+
+module Collectionspace
+  module Mapper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/collectionspace/mapper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Collectionspace::Mapper do
+  it "has a version number" do
+    expect(Collectionspace::Mapper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
