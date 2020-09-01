72400dd44c4d9dd52efce57dd43c9a1366913391
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-01 10:27:37 -0400

    cache auth and vocab terms for bonsai

--- spec/bonsai_helpers.rb
+++ spec/bonsai_helpers.rb
@@ -22,8 +22,17 @@ module Helpers
 
   def populate_bonsai(cache)
     terms = [
-      ['personauthorities', 'person', 'Ann Analyst', "urn:cspace:anthro.collectionspace.org:personauthorities:name(person):item:name(AnnAnalyst1594848799340)'Ann Analyst'"],
-      ['vocabularies', 'behrensmeyer', '2 - longitudinal cracks, exfoliation on surface', "urn:cspace:anthro.collectionspace.org:vocabularies:name(behrensmeyer):item:name(2)'2 - longitudinal cracks, exfoliation on surface'"], 
+      ['orgauthorities', 'organization', 'Bonsai Museum', "urn:cspace:bonsai.collectionspace.org:orgauthorities:name(organization):item:name(BonsaiMuseum1598919439027)'Bonsai Museum'"],
+      ['orgauthorities', 'organization', 'Bonsai Store', "urn:cspace:bonsai.collectionspace.org:orgauthorities:name(organization):item:name(BonsaiStore1598920297843)'Bonsai Store'"],
+      ['personauthorities', 'person', 'Ann Authorizer', "urn:cspace:bonsai.collectionspace.org:personauthorities:name(person):item:name(AnnAuthorizer1598919551068)'Ann Authorizer'"],
+      ['personauthorities', 'person', 'Debbie Depositor', "urn:cspace:bonsai.collectionspace.org:personauthorities:name(person):item:name(DebbieDepositor1598919493867)'Debbie Depositor'"],
+      ['personauthorities', 'person', 'Priscilla Plantsale', "urn:cspace:bonsai.collectionspace.org:personauthorities:name(person):item:name(PriscillaPlantsale1598920259864)'Priscilla Plantsale'"],
+      ['vocabularies', 'currency', 'Canadian Dollar', "urn:cspace:bonsai.collectionspace.org:vocabularies:name(currency):item:name(CAD)'Canadian Dollar'"],
+      ['vocabularies', 'dateera', 'CE', "urn:cspace:anthro.collectionspace.org:vocabularies:name(dateera):item:name(ce)'CE'"],
+      ['vocabularies', 'deaccessionapprovalgroup', 'collection committee', "urn:cspace:bonsai.collectionspace.org:vocabularies:name(deaccessionapprovalgroup):item:name(collection_committee)'collection committee'"],
+      ['vocabularies', 'deaccessionapprovalstatus', 'approved', "urn:cspace:bonsai.collectionspace.org:vocabularies:name(deaccessionapprovalstatus):item:name(approved)'approved'"],
+      ['vocabularies', 'deaccessionapprovalstatus', 'not required', "urn:cspace:bonsai.collectionspace.org:vocabularies:name(deaccessionapprovalstatus):item:name(not_required)'not required'"],
+      ['vocabularies', 'disposalmethod', 'public auction', "urn:cspace:bonsai.collectionspace.org:vocabularies:name(disposalmethod):item:name(public_auction)'public auction'"],
     ]
     populate(cache, terms)
   end
