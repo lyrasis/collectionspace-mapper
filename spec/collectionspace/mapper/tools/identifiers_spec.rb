4628d8490986236791eab2a40627061843116796
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-03 11:34:58 -0400

    revert error

--- spec/collectionspace/mapper/tools/identifiers_spec.rb
+++ spec/collectionspace/mapper/tools/identifiers_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe CollectionSpace::Mapper::Tools::Identifiers do
   describe '#short_identifier' do
     it 'generates identifier' do
-      name = "Jurgen Kloppp!"
+      name = "Jurgen Klopp!"
       short_identifier = "JurgenKlopp1289035554"
       expect(Identifiers.short_identifier(name)).to eq short_identifier
     end
