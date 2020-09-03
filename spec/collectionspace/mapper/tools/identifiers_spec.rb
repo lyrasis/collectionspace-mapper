bf4ce51e9714a79bf1a2cacf82fdb555d73cba2a
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-03 11:31:43 -0400

    introduce intentional error

--- spec/collectionspace/mapper/tools/identifiers_spec.rb
+++ spec/collectionspace/mapper/tools/identifiers_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe CollectionSpace::Mapper::Tools::Identifiers do
   describe '#short_identifier' do
     it 'generates identifier' do
-      name = "Jurgen Klopp!"
+      name = "Jurgen Kloppp!"
       short_identifier = "JurgenKlopp1289035554"
       expect(Identifiers.short_identifier(name)).to eq short_identifier
     end
