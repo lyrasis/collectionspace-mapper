7321dc46bbd25bb73ddf44c828b9c7aa9b8b176c
Kevin Kariuki <kariukim.kevin@gmail.com>
2020-09-04 14:44:54 +0300

    Test core loanin

--- spec/core_helpers.rb
+++ spec/core_helpers.rb
@@ -1,30 +1,49 @@
 # frozen_string_literal: true
 
 module Helpers
-    extend self
+  extend self
     
-    def core_client
-      CollectionSpace::Client.new(
-        CollectionSpace::Configuration.new(
-          base_uri: 'https://core.dev.collectionspace.org/cspace-services',
-          username: 'admin@core.collectionspace.org',
-          password: 'Administrator'
-        )
+  def core_client
+    CollectionSpace::Client.new(
+      CollectionSpace::Configuration.new(
+        base_uri: 'https://core.dev.collectionspace.org/cspace-services',
+        username: 'admin@core.collectionspace.org',
+        password: 'Administrator'
       )
-    end
+    )
+  end
     
-    def core_cache
-      cache_config = {
-        domain: 'core.collectionspace.org'
-      }
-      CollectionSpace::RefCache.new(config: cache_config, client: core_client)
-    end
-  
-    def populate_core(cache)
-      terms = [
-        ['personauthorities', 'person', 'Kev', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Kev1599058769862)'Kev'"],
-      ]
-      populate(cache, terms)
-    end
+  def core_cache
+    cache_config = {
+      domain: 'core.collectionspace.org'
+    }
+    CollectionSpace::RefCache.new(config: cache_config, client: core_client)
   end
   
+  def populate_core(cache)
+    terms = [
+      ['personauthorities', 'person', 'Kev', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Kev1599058769862)'Kev'"],
+    ]
+    populate(cache, terms)
+  end
+
+  def populate_core(cache)
+    terms = [
+      ['personauthorities', 'person', 'Karanja', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Karanja1599211015378)'Karanja'"],
+      ['personauthorities', 'person', 'Kimonda', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Kimonda1599211004900)'Kimonda'"],
+      ['vocabularies', 'loanoutstatus', 'Refused', "urn:cspace:core.collectionspace.org:vocabularies:name(loanoutstatus):item:name(refused)'Refused'"],
+      ['vocabularies', 'deaccessionapprovalgroup', 'collection committee', "urn:cspace:core.collectionspace.org:vocabularies:name(deaccessionapprovalgroup):item:name(collection_committee)'collection committee'"],
+      ['vocabularies', 'loanoutstatus', 'Returned', "urn:cspace:core.collectionspace.org:vocabularies:name(loanoutstatus):item:name(returned)'Returned'"],
+      ['personauthorities', 'person', 'John Kay', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(JohnKay1599210868122)'John Kay'"],
+      ['vocabularies', 'loanoutstatus', 'Authorized', "urn:cspace:core.collectionspace.org:vocabularies:name(loanoutstatus):item:name(authorized)'Authorized'"],
+      ['personauthorities', 'person', 'Loan', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Loan1599210896616)'Loan'"],
+      ['vocabularies', 'deaccessionapprovalgroup', 'executive committee', "urn:cspace:core.collectionspace.org:vocabularies:name(deaccessionapprovalgroup):item:name(executive_committee)'executive committee'"],
+      ['personauthorities', 'person', 'Kimani', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Kimani1599210926973)'Kimani'"],
+      ['personauthorities', 'person', 'James', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(James1599210943727)'James'"],
+      ['personauthorities', 'person', 'Henry', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Henry1599210937770)'Henry'"],
+      ['orgauthorities', 'organization', 'Sidarec', "urn:cspace:core.collectionspace.org:orgauthorities:name(organization):item:name(Sidarec1599210955079)'Sidarec'"],
+      ['personauthorities', 'person', 'Nyauma', "urn:cspace:core.collectionspace.org:personauthorities:name(person):item:name(Nyauma1599210983879)'Nyauma'"],            
+    ]
+    populate(cache, terms)
+  end
+end
