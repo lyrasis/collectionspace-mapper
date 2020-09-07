e9841139e26c4733f5af77d54021525ac882dff9
Kevin Kariuki <kariukim.kevin@gmail.com>
2020-09-07 12:34:43 +0300

    Test core media

--- spec/collectionspace/mapper/data_mapper_core_spec.rb
+++ spec/collectionspace/mapper/data_mapper_core_spec.rb
@@ -140,5 +140,36 @@ RSpec.describe CollectionSpace::Mapper::DataMapper do
         end
       end
     end
+
+    context 'media record' do
+      before(:all) do
+        @loanoutmapper = get_json_record_mapper(path: 'spec/fixtures/files/mappers/release_6_1/core/core_6_1_0-media.json')
+        @handler = DataHandler.new(record_mapper: @loanoutmapper, cache: @cache, client: core_client, config: @config)
+      end
+
+      context 'record 1' do
+        before(:all) do
+          @datahash = get_datahash(path: 'spec/fixtures/files/datahashes/core/media1.json')
+          @prepper = DataPrepper.new(@datahash, @handler)
+          @mapper = DataMapper.new(@prepper.prep, @handler, @prepper.xphash)
+          @mapped_doc = remove_namespaces(@mapper.response.doc)
+          @mapped_xpaths = list_xpaths(@mapped_doc)
+          @fixture_doc = get_xml_fixture('core/media1.xml')
+          @fixture_xpaths = test_xpaths(@fixture_doc, @handler.mapper[:mappings])
+        end
+       xit 'does not map unexpected fields' do
+          diff = @mapped_xpaths - @fixture_xpaths
+          expect(diff).to eq([])
+        end
+
+        xit 'maps as expected' do
+          @fixture_xpaths.each do |xpath| 
+            fixture_node = standardize_value(@fixture_doc.xpath(xpath).text)
+            mapped_node = standardize_value(@mapped_doc.xpath(xpath).text)
+            expect(mapped_node).to eq(fixture_node)
+          end
+        end
+      end
+    end
   end
 end
