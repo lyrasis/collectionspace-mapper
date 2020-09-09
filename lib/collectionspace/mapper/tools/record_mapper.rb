5ee674b559781f6b24fb16e8df851c31cdcdf087
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-09 10:49:44 -0400

    start building RecordMapper module

---
+++ lib/collectionspace/mapper/tools/record_mapper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module CollectionSpace
+  module Mapper
+    module Tools
+      module RecordMapper
+        ::RecordMapper = CollectionSpace::Mapper::Tools::RecordMapper
+        extend self
+
+        def convert(json)
+        end
+
+      end
+    end
+  end
+end
