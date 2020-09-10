434f427efa418ba65ccf6722b42923dfbe69d38b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-10 12:21:23 -0400

    sort cache population instructions

--- spec/botgarden_helpers.rb
+++ spec/botgarden_helpers.rb
@@ -24,18 +24,18 @@ module Helpers
 
   def populate_botgarden(cache)
     terms = [
+      ['citationauthorities', 'citation', 'FNA Volume 19', "urn:cspace:botgarden.collectionspace.org:citationauthorities:name(citation):item:name(FNAVolume191599238760383)'FNA Volume 19'"],
       ['citationauthorities', 'citation', 'Sp. Pl. 2: 899. 1753', "urn:cspace:botgarden.collectionspace.org:citationauthorities:name(citation):item:name(SpPl289917531599238184211)'Sp. Pl. 2: 899. 1753'"],
       ['citationauthorities', 'worldcat', 'Bull. Torrey Bot. Club', "urn:cspace:botgarden.collectionspace.org:citationauthorities:name(worldcat):item:name(BullTorreyBotClub331599245358364)'Bull. Torrey Bot. Club 33'"],
-      ['citationauthorities', 'citation', 'FNA Volume 19', "urn:cspace:botgarden.collectionspace.org:citationauthorities:name(citation):item:name(FNAVolume191599238760383)'FNA Volume 19'"],
       ['orgauthorities', 'organization', 'FVA', "urn:cspace:botgarden.collectionspace.org:orgauthorities:name(organization):item:name(FVA1599246022216)'FVA'"],
       ['personauthorities', 'person', 'Linnaeus, Carl', "urn:cspace:botgarden.collectionspace.org:personauthorities:name(person):item:name(LinnaeusCarl1599238374086)'Linnaeus, Carl'"],
+      ['taxonomyauthority', 'taxon', 'Domestic', "urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(Domestic1599750187683)'Domestic'"],
+      ['taxonomyauthority', 'taxon', 'Tropez', "urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(Tropez1599750195530)'Tropez'"],
       ['vocabularies', 'languages', 'English', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(languages):item:name(eng)'English'"],
       ['vocabularies', 'languages', 'French', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(languages):item:name(fra)'French'"],
       ['vocabularies', 'languages', 'Latin', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(languages):item:name(lat)'Latin'"],
-      ['vocabularies', 'taxontermflag', 'valid', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(taxontermflag):item:name(valid)'valid'"],
       ['vocabularies', 'taxontermflag', 'invalid', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(taxontermflag):item:name(invalid)'invalid'"],
-      ['taxonomyauthority', 'taxon', 'Tropez', "urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(Tropez1599750195530)'Tropez'"],
-      ['taxonomyauthority', 'taxon', 'Domestic', "urn:cspace:botgarden.collectionspace.org:taxonomyauthority:name(taxon):item:name(Domestic1599750187683)'Domestic'"],
+      ['vocabularies', 'taxontermflag', 'valid', "urn:cspace:botgarden.collectionspace.org:vocabularies:name(taxontermflag):item:name(valid)'valid'"],
     ]
     populate(cache, terms)
   end
