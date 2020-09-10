01f6fbf671fbf2d5000f722d8fe63b2e65bc8e4d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-09-10 12:34:10 -0400

    acquisition test was in file twice; fix variable names

--- spec/collectionspace/mapper/data_mapper_core_spec.rb
+++ spec/collectionspace/mapper/data_mapper_core_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe CollectionSpace::Mapper::DataMapper do
 
     context 'acquisition record' do
       before(:all) do
-        @loanoutmapper = get_json_record_mapper(path: 'spec/fixtures/files/mappers/release_6_1/core/core_6_1_0-acquisition.json')
-        @handler = DataHandler.new(record_mapper: @loanoutmapper, cache: @cache, client: core_client, config: @config)
+        @acquisition_mapper = get_json_record_mapper(path: 'spec/fixtures/files/mappers/release_6_1/core/core_6_1_0-acquisition.json')
+        @handler = DataHandler.new(record_mapper: @acquisition_mapper, cache: @cache, client: core_client, config: @config)
       end
 
       context 'record 1' do
@@ -350,36 +350,6 @@ RSpec.describe CollectionSpace::Mapper::DataMapper do
         it 'maps as expected' do
           @fixture_xpaths.each do |xpath|
             next if xpath.start_with?('/document/objectexit_common/exitDateGroup')
-            fixture_node = standardize_value(@fixture_doc.xpath(xpath).text)
-            mapped_node = standardize_value(@mapped_doc.xpath(xpath).text)
-            expect(mapped_node).to eq(fixture_node)
-          end
-        end
-      end
-    end
-        context 'acquisition record' do
-      before(:all) do
-        @movementmapper = get_json_record_mapper(path: 'spec/fixtures/files/mappers/release_6_1/core/core_6_1_0-acquisition.json')
-        @handler = DataHandler.new(record_mapper: @movementmapper, cache: @cache, client: core_client, config: @config)
-      end
-
-      context 'record 1' do
-        before(:all) do
-          @datahash = get_datahash(path: 'spec/fixtures/files/datahashes/core/acquisition1.json')
-          @prepper = DataPrepper.new(@datahash, @handler)
-          @mapper = DataMapper.new(@prepper.prep, @handler, @prepper.xphash)
-          @mapped_doc = remove_namespaces(@mapper.response.doc)
-          @mapped_xpaths = list_xpaths(@mapped_doc)
-          @fixture_doc = get_xml_fixture('core/acquisition1.xml')
-          @fixture_xpaths = test_xpaths(@fixture_doc, @handler.mapper[:mappings])
-        end
-        it 'does not map unexpected fields' do
-          diff = @mapped_xpaths - @fixture_xpaths
-          expect(diff).to eq([])
-        end
-
-        it 'maps as expected' do
-          @fixture_xpaths.each do |xpath|
             fixture_node = standardize_value(@fixture_doc.xpath(xpath).text)
             mapped_node = standardize_value(@mapped_doc.xpath(xpath).text)
             expect(mapped_node).to eq(fixture_node)
