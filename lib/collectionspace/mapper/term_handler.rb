6535f05c8ea2a82a3543c0cbe646f3e1aee0eab3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-02-11 19:45:49 -0500

    Warn instead of failing when... - a Client term search returns more than one record matching the term. - when incorrect delimiting in repeating field subgroups causes overflow (more subgroups than there are parent groups) Also warn when subgroup values are uneven

--- lib/collectionspace/mapper/term_handler.rb
+++ lib/collectionspace/mapper/term_handler.rb
@@ -3,7 +3,9 @@
 module CollectionSpace
   module Mapper
     class TermHandler
-      attr_reader :result, :terms
+      attr_reader :result, :terms, :warnings, :errors,
+        :column, :source_type, :type, :subtype
+      attr_accessor :value
       def initialize(mapping:, data:, client:, cache:, config:)
         @mapping = mapping
         @column = mapping[:datacolumn]
@@ -23,6 +25,8 @@ module CollectionSpace
           @type = 'vocabularies'
           @subtype = @mapping[:transforms][:vocabulary]
         end
+        @warnings = []
+        @errors = []
         handle_terms
       end
 
@@ -37,12 +41,14 @@ module CollectionSpace
       end
 
       def handle_term(val)
+        @value = val
+        
         return '' if val.blank?
         added = false
         
         term_report = {
-          category: @source_type,
-          field: @column
+          category: source_type,
+          field: column
         }
 
         if in_cache?(val)
@@ -63,44 +69,66 @@ module CollectionSpace
 
         unless added
           refname_obj = CollectionSpace::Mapper::Tools::RefName.new(
-            source_type: @source_type,
-            type: @type,
-            subtype: @subtype,
+            source_type: source_type,
+            type: type,
+            subtype: subtype,
             term: val,
             cache: @cache)
           @terms << term_report.merge({ found: false, refname: refname_obj })
-          @cache.put(@type, @subtype, val, refname_obj.urn)
+          @cache.put(type, subtype, val, refname_obj.urn)
           refname_urn = refname_obj.urn
         end
         refname_urn
       end
 
       def in_cache?(val)
-        @cache.exists?(@type, @subtype, val)
+        @cache.exists?(type, subtype, val)
       end
 
       def cached_term(val)
-        @cache.get(@type, @subtype, val, search: false)
+        @cache.get(type, subtype, val, search: false)
       end
 
       def searched_term(val)
         begin
           response = @client.find(
-            type: @type,
-            subtype: @subtype,
+            type: type,
+            subtype: subtype,
             value: val,
             field: search_field
           )
         rescue StandardError => e
           puts e.message
         else
-          response.parsed.dig('abstract_common_list', 'list_item', 'refName')
+          response_term_refname(response)
         end
       end
 
+      def response_term_refname(response)
+        term_ct = response.parsed.dig('abstract_common_list', 'totalItems')
+        return nil if term_ct.nil?
+
+        if term_ct.to_i == 1
+          refname = response.parsed.dig('abstract_common_list', 'list_item', 'refName')
+        elsif term_ct.to_i > 1
+          rec = response.parsed.dig('abstract_common_list', 'list_item')[0]
+          using_uri = "#{@client.config.base_uri}#{rec['uri']}"
+          refname = rec['refName']
+          warnings << {
+            category: :multiple_records_found_for_term,
+            field: column,
+            type: type,
+            subtype: subtype,
+            value: value,
+            message: "#{term_ct} records found. Using #{using_uri}"
+          }
+        end
+        refname
+      end
+      
       def search_field
         begin
-          field = CollectionSpace::Service.get(type: @type)[:term]
+          field = CollectionSpace::Service.get(type: type)[:term]
         rescue StandardError => e
           puts e.message
         else
