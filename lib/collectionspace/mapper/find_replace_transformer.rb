6ef229ce98746149830281b6ded1839a68ebc7d7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-04-07 21:11:09 -0400

    complete FindReplaceTransformer with embedded queue of operations

--- lib/collectionspace/mapper/find_replace_transformer.rb
+++ lib/collectionspace/mapper/find_replace_transformer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'transformer'
+require_relative 'find_replace_operation'
 
 module CollectionSpace
   module Mapper
@@ -9,9 +10,15 @@ module CollectionSpace
     class FindReplaceTransformer < Transformer
       def initialize(transform:)
         super
+        @operations = transform.map{ |operation| FindReplaceOperation.create(operation) }
       end
       
       def transform(value)
+        return value if value.blank?
+
+        newval = value
+        @operations.each{ |operation| newval = operation.perform(newval) }
+        newval
       end
     end
   end
